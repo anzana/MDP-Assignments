25c3df6a8cbceaf9a69c9e68088d3035f1334e8c
Anjana Sharma <anzana.sharma@gmail.com>
2022-10-23 18:26:13 -0500

    initial modifications

--- app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dinnerdecider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val foodList = arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
@@ -11,25 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        addFoodButton.setOnClickListener {
-//        }
 
-
-//        addFoodBtn.setOnClickListener {
-//            if (addNewFoodEditText.text.isEmpty()) {
-//                Toast.makeText(this, "Please enter a food", Toast.LENGTH_SHORT).show()
-//                return@setOnClickListener
-//            }
-//            val newFood = addNewFoodEditText.text.toString()
-//            foodList.add(newFood)
-//            addNewFoodEditText.text.clear()
-//            println(foodList)
-//            Toast.makeText(this, "Added $newFood", Toast.LENGTH_SHORT).show()
-//        }
-//        decideBtn.setOnClickListener {
-//            val random = Random()
-//            val randomFood = random.nextInt(foodList.count())
-//            dinnerText.text = foodList[randomFood]
+        addFoodButton.setOnClickListener {
+            if (addNewFoodEditText.text.isEmpty()) {
+                Toast.makeText(this, "Please enter a food", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val newFood = addNewFoodEditText.text.toString()
+            foodList.add(newFood)
+            addNewFoodEditText.text.clear()
+            println(foodList)
+            Toast.makeText(this, "Added $newFood", Toast.LENGTH_SHORT).show()
+      }
+        decideButton.setOnClickListener {
+          val random = Random()
+          val randomFood = random.nextInt(foodList.count())
+         dinnerText.text = foodList[randomFood]
         }
-    }
+  }
 }
